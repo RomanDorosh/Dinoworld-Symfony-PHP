8d0a899895a29a2de5beec53e2747a55c8544519
RomanDorosh <roma.dorosh@gmail.com>
2021-03-25 21:42:33 +0100

    Added a function for add a new dino to the db

--- src/Controller/RegisterController.php
+++ src/Controller/RegisterController.php
@@ -2,9 +2,14 @@
 
 namespace App\Controller;
 
+use App\Entity\Dinosaur;
 use App\Entity\User;
 use App\Form\UserType;
 use App\Repository\UserRepository;
+use App\Repository\PeriodRepository;
+use App\Repository\ContinentRepository;
+use App\Repository\DietRepository;
+use App\Repository\DinosaurRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\Request;
@@ -21,12 +26,11 @@ class RegisterController extends AbstractController
 {
 
     /**
-     * @Route("", name="user_register", methods={"GET","POST"})
+     * @Route("/user", name="user_register", methods={"GET","POST"})
      */
-    public function register(Request $request, EntityManagerInterface $em, UserPasswordEncoderInterface $encoder): Response
+    public function registerUser(Request $request, EntityManagerInterface $em, UserPasswordEncoderInterface $encoder): Response
     {
         $userData = json_decode($request->getContent());
-        dump($userData);
         $user = new User;
         $user->setEmail($userData->username);
         $password = $encoder->encodePassword($user, $userData->password);
@@ -42,5 +46,52 @@ class RegisterController extends AbstractController
 
 
         return new JsonResponse($user);
+    }
+
+    /**
+     * @Route("/dinosaur", name="dinosaur_register", methods={"GET","POST"})
+     */
+    public function registerDinosaur(Request $request, EntityManagerInterface $em, PeriodRepository $periodRepository, DietRepository $dietRepository, ContinentRepository $continentRepository): Response
+    {
+        $name = $request->request->get("name");
+        $periodId = (int) $request->request->get("period");
+        $dietId = (int) $request->request->get("diet");
+        $continentId = (int) $request->request->get("continent");
+        $weight = (int) $request->request->get("weight");
+        $height = (int) $request->request->get("height");
+        $lenght = (int) $request->request->get("lenght");
+        $top_speed = (int) $request->request->get("top_speed");
+        $top = (int) $request->request->get("top");
+        $info = $request->request->get("info");
+        $img = $request->files->get("img");
+
+        // dump($img);
+
+        $directory = '/var/www/html/finalsymfonyproject/public/images';
+
+        $fileName = "$name.jpg";
+
+        $img->move($directory, $fileName);
+
+
+        $dinosaur = new Dinosaur();
+        $dinosaur->setName($name);
+        $dinosaur->setPeriod($periodRepository->find($periodId));
+        $dinosaur->setDiet($dietRepository->find($dietId));
+        $dinosaur->setContinent($continentRepository->find($continentId));
+        $dinosaur->setWeight($weight);
+        $dinosaur->setHeight($height);
+        $dinosaur->setLenght($lenght);
+        $dinosaur->setTopSpeed($top_speed);
+        $dinosaur->setTop($top);
+        $dinosaur->setInfo($info);
+        $dinosaur->setImg($fileName);
+
+        $em->persist($dinosaur);
+        $em->flush(); //INSERT to Mysql
+
+
+
+        return new JsonResponse([]);
     }
 }
